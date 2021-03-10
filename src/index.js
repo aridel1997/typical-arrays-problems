f37cc3274e0557dc5b164485c46fa59fe179ae16
aridel1997 <arinadelendik1997@gmail.com>
2021-03-10 20:29:38 +0300

    feat: add find max/min/avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
-exports.min = function min (array) {
+exports.min = function min(array) {
+  if (array != undefined) {
+    if (array.length != 0) {
+      let minElement = array[0];
+      for (let i = 0; i < array.length; i++) {
+        if (minElement > array[i]) {
+          minElement = array[i];
+        }
+      }
+      return minElement;
+    }
+  }
   return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array != undefined) {
+    if (array.length != 0) {
+      let maxElement = array[0];
+      for (let i = 0; i < array.length; i++) {
+        if (maxElement < array[i]) {
+          maxElement = array[i];
+        }
+      }
+      return maxElement;
+    }
+  } return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array != undefined) {
+    if (array.length != 0) {
+      let arithmeticMean = 0;
+      for (let i = 0; i < array.length; i++) {
+        arithmeticMean += array[i];
+      }
+      return arithmeticMean / array.length;
+    }
+  } return 0;
 }
